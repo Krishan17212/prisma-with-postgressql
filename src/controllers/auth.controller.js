f35b502abc3d0f89bd58a31cbc956ed1f57b7cbb
Krishan17212 <kkprabhakar1008@gmail.com>
2026-06-16 22:51:04 +0530

    created authcontroller and successfully added contact

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,42 @@
+import bcrypt from "bcrypt";
+import { prisma } from "../config/db.js";
+
 const register = async (req, res) => {
   try {
-    res.json({ message: "Auth router is working" });
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: `All fields ["Email", "Name", "Password"] are required`,
+      });
+    }
+
+    const userExists = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: `User with email ${email} already exists`,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+    res.status(201).json({
+      message: "User registered successfully",
+      data: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
