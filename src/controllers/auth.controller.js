da8a2476e7a243fbf1c118d70246dd06f93a7805
Krishan17212 <kkprabhakar1008@gmail.com>
2026-06-18 21:59:02 +0530

    add logout function in auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -81,4 +81,14 @@ const login = async (req, res) => {
   }
 };
 
-export { register, login };
+const logout = async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({
+    message: "User logged out successfully",
+  });
+};
+
+export { register, login, logout };
