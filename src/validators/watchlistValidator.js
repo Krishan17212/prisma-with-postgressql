4fa246b09289e39aaa0642606874483e37989589
Krishan17212 <kkprabhakar1008@gmail.com>
2026-06-23 21:53:37 +0530

    add updateWatchlistSchema in watchlistValidator

--- src/validators/watchlistValidator.js
+++ src/validators/watchlistValidator.js
@@ -16,3 +16,19 @@ const addWatchlistSchema = z.object({
   notes: z.string().optional(),
   notes: z.string().optional,
 });
+
+const updateWatchlistSchema = z.object({
+  id: z.string().uuid(),
+  status: z
+    .enum(["COMPLETED", "WATCHING", "DROPPED", "PLANNED"], {
+      message: "Status is required",
+    })
+    .optional(),
+  rating: z.coerce
+    .number()
+    .int("Rating must be an integer")
+    .min(1, "Rating must be at least 1")
+    .max(10, "Rating must be at most 10")
+    .optional(),
+  notes: z.string().optional(),
+});
