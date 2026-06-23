a52669ffc49abb5eab2cf0c6e8676f4e951e8387
Krishan17212 <kkprabhakar1008@gmail.com>
2026-06-23 21:53:06 +0530

    add watchlistschema function in watchlistValidator

---
+++ src/validators/watchlistValidator.js
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+const addWatchlistSchema = z.object({
+  movieId: z.string().uuid(),
+  status: z
+    .enum(["COMPLETED", "WATCHING", "DROPPED", "PLANNED"], {
+      message: "Status is required",
+    })
+    .optional(),
+  rating: z.coerce
+    .number()
+    .int("Rating must be an integer")
+    .min(1, "Rating must be at least 1")
+    .max(10, "Rating must be at most 10")
+    .optional(),
+  notes: z.string().optional(),
+  notes: z.string().optional,
+});
